c4522998ea4df203690d9ba11f95b02f7268d760
xiaoguanqiu <xiaoguanqiu@example.com>
2021-08-04 10:07:11 +0800

    [detail:缓存机制与异步API的async和await]

--- pages/post-detail/post-detail.js
+++ pages/post-detail/post-detail.js
@@ -6,17 +6,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-    postData:{}
+    collect:false,
+    postData:{},
+    _pid:null,
+    _postCollect : {}
+  },
+
+  onCollect(event){
+    const pid = this.data._pid;
+    let postCollect = this.data._postCollect;
+
+    if (!postCollect[pid]){
+      postCollect[pid] = true;
+    }else{
+      postCollect[pid] = false;
+    }
+    wx.showToast({
+      title: postCollect[pid]?'收藏成功':'取消收藏',
+    })
+    this.setData({
+      collect: postCollect[pid]
+    })
+    this.data._postCollect = postCollect
+    console.log('postCollect:',postCollect)
+    wx.setStorageSync('postCollect', postCollect)
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
-    const postData = postList[options.pid];
+    const postData =  postList[options.pid]
+    this.data._pid = options.pid
+    const postCollect = wx.getStorageSync('postCollect')
+    //必须判断是否有值才赋值，否则不能this.data._postCollect[]使用
+    if (postCollect){
+      this.data._postCollect = postCollect
+    }
     this.setData({
-      postData
+      postData,
+      collect: postCollect[options.pid]
     })
   },
 
